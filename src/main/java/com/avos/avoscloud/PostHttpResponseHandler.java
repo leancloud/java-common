a3591f3bf66053489065536043221469b408546e
lbt05 <lbt0506@gmail.com>
2016-06-24 15:04:09 +0800

    AVException parse logic

--- src/main/java/com/avos/avoscloud/PostHttpResponseHandler.java
+++ src/main/java/com/avos/avoscloud/PostHttpResponseHandler.java
@@ -24,7 +24,7 @@ public class PostHttpResponseHandler extends AsyncHttpResponseHandler {
     int code = AVErrorUtils.errorCode(content);
     if (code > 0) {
       if (getCallback() != null) {
-        getCallback().onFailure(AVErrorUtils.createException(code, content), content);
+        getCallback().onFailure(AVErrorUtils.createException(content), content);
       }
       return;
     }
